8c0474d0ecc2c6264094fa06300c9c197a6a7dcd
zras05 <37888815+zras05@users.noreply.github.com>
2018-04-18 11:29:57 +0800

    子组件

---
+++ communication/VueCommunication.vue
@@ -0,0 +1,46 @@
+<!-- 
+	父子组件通信--子组件
+	Created by zras 
+	Time 2017-09-27
+ -->
+
+<template>
+	<div class="VueCommunication">
+		<p>子组件</p>
+		<input type="text" placeholder="vue prop $emit 子组件" v-model="vueValue" @input="sentToFather"> 
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'bottom',
+	props: ['msg'],
+	data () {
+		return {
+			vueValue: ''
+		}
+	},
+	watch: {
+		msg (val) {
+			this.vueValue = val
+		}
+	},
+	methods: {
+		sentToFather () {
+			this.$emit('getMsg', this.vueValue)	
+		}
+	},
+	mounted () {
+	}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+	p {
+		padding-left: 20px;
+	}
+	input {
+		margin-left: 20px;
+	}
+</style>
---
+++ communication/VuexCommunication.vue
@@ -0,0 +1,50 @@
+<!-- 
+	vuex store 组件间通信
+	Created by zras 
+	Time 2017-09-27
+ -->
+
+<template>
+	<div class="vuexCommunication">
+		<p>子组件</p>
+		<input type="text" placeholder="vuex store 子组件" v-model="vuexValue" @input="sentToFather">
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'bottom',
+	data () {
+		return {
+			vuexValue: ''
+		}
+	},
+	computed: {  
+		fatherchange () {  
+			return this.$store.state.storemsg
+		}  
+	},  
+	watch: {  
+		fatherchange: function (value, old) {  
+			this.vuexValue = value
+		}  
+	}, 
+	methods: {
+		sentToFather () {
+			this.$store.commit('getMsg', this.vuexValue)
+		}
+	},
+	mounted () {
+	}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+	p {
+		padding-left: 20px;
+	} 
+	input {
+		margin-left: 20px;
+	}
+</style>
