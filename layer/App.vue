ccd8baf9041f931e2c7281b8892e8769ab6bd914
zras05 <37888815+zras05@users.noreply.github.com>
2018-06-08 15:00:19 +0800

    vue弹窗组件
    vue弹窗组件

---
+++ layer/App.vue
@@ -0,0 +1,80 @@
+<template>
+  <div id="app">
+	  <p @click="loading()">loading</p>
+	  <p @click="msg">msg</p>
+	  <p @click="alert">alert</p>
+	  <p @click="confirm">confirm</p>
+	  <Layer :layer='layer' @close="closeAlert" @confirm="confirmAlert"></Layer>
+  </div>
+</template>
+
+<script>
+import Layer from '@/components/Layer'
+export default {
+	name: 'App',
+	data () {
+		return {
+			layer: {
+			}
+		}
+	},
+	components: {
+		Layer
+	},
+	methods: {
+		confirmAlert () {
+			this.layer = {}
+		},
+		closeAlert () {
+			this.layer = {}
+		},
+		loading () {
+			this.layer = {
+				type: 'loading'
+			}
+			setTimeout(() => {
+				this.layer = {
+					show: false
+				}
+			}, 3000)
+		},
+		msg () {
+			this.layer = {
+				type: 'msg',
+				msg: ['提示提示提示']
+			}
+
+		},
+		alert () {
+			this.layer = {
+				type: 'alert',
+				title: '标题',
+				msg: ['提示提示提示']
+			}
+
+		},
+		confirm () {
+			this.layer = {
+				type: 'confirm',
+				title: '标题',
+				msg: ['提示提示提示'],
+				btns: ['取消', '确认']
+			}
+		}
+	},
+	mounted () {
+	}
+}
+</script>
+
+<style>
+html, body {
+	margin: 0;
+	padding: 0;
+	height: 100%;
+}
+#app {
+	height: 100%;
+	overflow: hidden;
+}
+</style>
