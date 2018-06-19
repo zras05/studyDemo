809aca9d03af56a35a2d6cb0ecb4febac43f4053
zras <591033129@qq.com>
2018-06-19 12:14:48 +0800

    delete

--- layer/components/layer/layerjs.js
+++
@@ -1,9 +0,0 @@
-import Layer from './Layer'
-export default {
-	install: function (Vue, opt) {
-		Vue.component('Layer', Layer)
-		Vue.prototype.closeAlert = function (layer) {
-			layer = {}
-		}
-	}
-}
